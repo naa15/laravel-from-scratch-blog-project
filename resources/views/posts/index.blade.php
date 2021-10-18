74f745b8727ca97c2330bf7632d4322f9295eaeb
naa15 <naltu15@freeuni.edu.ge>
2021-10-18 15:11:48 +0400

    feat: add pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,6 +8,8 @@
         @if ($posts->count())
             <x-posts-grid :posts="$posts" />
 
+            {{ $posts->links() }}
+
         @else
             <p class="text-center">No posts yet. Come back later.</p>
         @endif
