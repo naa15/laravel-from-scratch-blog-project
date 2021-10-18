efd7380b307265f0c9f1bcf0644d56f02ff4c74f
naa15 <naltu15@freeuni.edu.ge>
2021-10-18 18:28:05 +0400

    feat: build a register user page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
 /*
 | These routes are loaded by the RouteServiceProvider within a group which
@@ -12,3 +13,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
