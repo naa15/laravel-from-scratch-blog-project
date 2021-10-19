8e1287041166e4a21fe1a051218f874b53abe64f
naa15 <naltu15@freeuni.edu.ge>
2021-10-19 13:36:13 +0400

    feat: add validation to registration form

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -15,8 +15,13 @@ class RegisterController extends Controller
     {
         $attributes = request()->validate([
             'name' => 'required|max:255',
-            'username' => 'required|min:3|max:255',
-            'email' => ['required', 'email', 'max:255'],
+            'username' => 'required|min:3|max:255|unique:users,username',
+            /*
+            * other way of doing the same thing: 
+            * 'username' => ['required', 'min:3', 'max:255', Rule::unique('users', 'username')],
+            * 'email' => ['required', 'email', 'max:255'],
+            */
+            'email' => 'required|email|max:255|unique:users,email',
             'password' => 'required|min:7|max:255'
         ]);
         
