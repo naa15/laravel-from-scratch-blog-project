11c43faade18c94669f3d4c7e65e8daaf39893a0
naa15 <naltu15@freeuni.edu.ge>
2021-10-21 17:51:33 +0400

    feat: add 'leave comment' form

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -65,6 +65,7 @@
             </div>
         </footer>
     </section>
+    
     <x-flash />
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use App\Http\Controllers\CommentController;
 
 /*
 | These routes are loaded by the RouteServiceProvider within a group which
@@ -14,6 +15,7 @@ use App\Http\Controllers\SessionsController;
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+Route::post('posts/{post:slug}/comments', [CommentController::class, 'store']); 
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
