3878b1bdae6a6f6010a2d976b86ef5140d8be867
naa15 <naltu15@freeuni.edu.ge>
2021-12-16 19:57:43 +0400

    refactor: change hard-coded routes to named routes

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -14,7 +14,7 @@
                                         <td class="px-6 py-4 whitespace-nowrap">
                                             <div class="ml-4">
                                                 <div class="text-sm font-medium text-gray-900">
-                                                    <a href="/posts/{{ $post->slug }}">{{ ucwords($post->title) }}</a>
+                                                    <a href="{{ route('show', $post->slug) }}">{{ ucwords($post->title) }}</a>
                                                 </div>
                                             </div>
                                         </td>
--- resources/views/components/setting.blade.php
+++ resources/views/components/setting.blade.php
@@ -10,13 +10,13 @@
             <h4 class="font-semibold mb-4">Links</h4>
             <ul>
                 <li>
-                    <a href="{{ route('admin.posts') }}" class="{{ request()->is('admin/posts') ? 'text-blue-500' : '' }}">All
+                    <a href="{{ route('admin.posts') }}" class="{{ request()->routeIs('admin.posts') ? 'text-blue-500' : '' }}">All
                         Posts</a>
                 </li>
 
                 <li>
                     <a href="{{ route('admin.posts.create') }}"
-                        class="{{ request()->is('admin/posts/create') ? 'text-blue-500' : '' }}">New post</a>
+                        class="{{ request()->routeIs('admin.posts.create') ? 'text-blue-500' : '' }}">New post</a>
                 </li>
             </ul>
         </aside>
